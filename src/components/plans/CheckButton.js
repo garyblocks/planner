2be36bfde4498fe70fc0487832dc3daa9ad2e9cf
gary wang <gary@equity.com>
2020-06-23 20:52:05 -0500

    save complete to log

--- src/components/plans/CheckButton.js
+++ src/components/plans/CheckButton.js
@@ -5,19 +5,35 @@ import { togglePlan } from "../../redux/actions";
 import { API_URL } from '../../constants';
 
 const CheckButton = ({ plan, togglePlan }) => {
+
+    const handleClick = (event) => {
+        event.stopPropagation();
+        const plan_id = plan.id
+        console.log(plan);
+        if (plan.completed) {
+            const data = {
+                fields: { done: false }
+            }
+            axios.patch(API_URL + 'planner/plans/' + plan_id, {data}, {withCredentials: true})
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+                togglePlan(plan_id);
+            });
+        } else {
+            const data = { plan_id }
+            axios.post(API_URL + 'planner/complete_plan', {data}, {withCredentials: true})
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+                togglePlan(plan_id);
+            });
+        }
+    } 
+
     return (
         <i 
-            onClick={(event) => {
-                event.stopPropagation();
-                const plan_id = plan.id
-                const data = { plan_id }
-                axios.post(API_URL + 'planner/toggle_plan', {data}, {withCredentials: true})
-                .then(res => {
-                    console.log(res);
-                    console.log(res.data);
-                    togglePlan(plan_id);
-                });
-            }}
+            onClick={handleClick}
             className="material-icons"
         >check_circle</i>
     );
